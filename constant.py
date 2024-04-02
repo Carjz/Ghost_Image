288f493d8a5ea0661c3f459bb2cf4baede00058e
Carjz <1904258420@qq.com>
2024-04-02 19:52:29 +0800

    Update

--- constant.py
+++ constant.py
@@ -3,16 +3,17 @@ from math import ceil
 import torchvision.transforms as transforms
 
 
-DATASET_DIR = "Inputs/CVPR"
+DATASET_DIR = "Inputs/Caltech_256"
 FOLDER_PATH = "Outputs"
 IMAGE_SIZE = 200
-BATCH_SIZE = 100
+BATCH_SIZE = 64
 
-num_epochs = 50
+num_epochs = 3
 Nyquist_rate = IMAGE_SIZE * IMAGE_SIZE
 sampling_times = ceil(Nyquist_rate * 0.01)
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device_id = 0
+device = torch.device(f"cuda:{device_id}" if torch.cuda.is_available() else "cpu")
 gpus = torch.cuda.device_count()
 
 transform = transforms.Compose(
--- make_dataset_dir.py
+++ make_dataset_dir.py
@@ -1,28 +1,18 @@
+#!/bin/python3
+
 import os
 import shutil
 import fnmatch
 
 def move_files(src_dir, dst_dir, file_pattern):
-    """
-    将符合给定模式的文件从源目录及其子目录移动到目标目录
-    
-    Args:
-        src_dir (str): 源目录路径
-        dst_dir (str): 目标目录路径
-        file_pattern (str): 文件名模式(如 "*.jpg")
-    """
-    # 删除目标目录(如果存在)
-    if os.path.exists(dst_dir):
-        shutil.rmtree(dst_dir)
     os.makedirs(f"{dst_dir}/train")
     os.makedirs(f"{dst_dir}/test")
     
-    # 遍历源目录及其子目录
     for root, dirs, files in os.walk(src_dir):
         for file in files:
             _, dir_name = os.path.split(root)
             src_file = os.path.join(root, file)
-            # 检查文件名是否符合模式
+
             if fnmatch.fnmatch(file, file_pattern):
                 cur_dst = f"{dst_dir}/test/{dir_name}"
             else:
@@ -31,9 +21,7 @@ def move_files(src_dir, dst_dir, file_pattern):
                 os.makedirs(cur_dst)
             dst_file = os.path.join(cur_dst, file)
                 
-            # 移动文件
             shutil.move(src_file, dst_file)
             print(f"Moved '{src_file}' to '{dst_file}'")
 
-# 使用示例
-move_files("Inputs/Images", "Inputs/CVPR", "*0.jpg")
+move_files("Inputs/Caltech_256", "Inputs/Caltech_256", "*0.jpg")
